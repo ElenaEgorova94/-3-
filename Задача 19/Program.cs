3d572cdceade076115fe1ef65d9d45ca9dc3bac6
ElenaEgorova <lenny_94@mail.ru>
2022-12-05 22:33:32 +0300

    Добавьте файлы проекта.

---
+++ Задача 19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string N = Console.ReadLine();
+
+if (N[0] == N[4] && N[1] == N[3])
+{
+    Console.WriteLine($"Ваше число {N} - палиндром");
+}
+
+else
+{
+    Console.WriteLine($"Ваше число {N} - не палиндром");
+}
